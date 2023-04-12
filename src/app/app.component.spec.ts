17a912927a374a17439df89eac5e2dedc714582b
Donovan Griggs <donovan@iiimpact.io>
2023-04-12 22:21:20 +0200

    calculator

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,9 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { AppComponent, Operator } from './app.component';
 
-describe('AppComponent', () => {
+describe('FilterPanelComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -10,22 +12,82 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+  });
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculator');
-  });
+  it('getNumber() should get correct number from input', () => {
+    component.getNumber('0')
+    component.getNumber('1')
+    component.getNumber('2')
+    component.getNumber('3')
+    expect(component.currentNumber).toBe('123')
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('calculator app is running!');
-  });
+  it('addDecimal() should add decimal to number', () => {
+    component.getNumber('1')
+    component.addDecimal()
+    component.getNumber('5')
+    expect(component.currentNumber).toBe('1.5')
+  })
+
+  it('doCalculation() should do the correct calculation based on firstOperand, secondOperand and operator', () => {
+    component.firstOperand = 10
+    expect(component.doCalculation(Operator.Add, 10)).toEqual(20)
+    component.firstOperand = 10
+    expect(component.doCalculation(Operator.Subtract, 10)).toEqual(0)
+    component.firstOperand = 10
+    expect(component.doCalculation(Operator.Multiply, 10)).toEqual(100)
+    component.firstOperand = 10
+    expect(component.doCalculation(Operator.Divide, 10)).toEqual(1)
+  })
+
+  it('doCalculation() should return null if no firstOperand', () => {
+    expect(component.firstOperand).toBeNull()
+    expect(component.doCalculation(Operator.Add, 10)).toBeNull()
+  })
+
+  it('getOperation() should get operator and pause for second operand', () => {
+    component.getNumber('1')
+    expect(component.firstOperand).toBeNull()
+    component.getOperation(Operator.Add)
+    expect(component.firstOperand).toBeDefined()
+    expect(component.operator).toBe(Operator.Add)
+    expect(component.pauseForSecondNumber).toBeTrue()
+  })
+
+  it('getOperation() should call doCalculation()', () => {
+    spyOn(component, 'doCalculation')
+    component.getNumber('1')
+    component.getOperation(Operator.Add)
+    component.getNumber('1')
+    component.getOperation(Operator.Equals)
+    expect(component.doCalculation).toHaveBeenCalled()
+  })
+
+  it('clear() should clear the calculator', () => {
+    component.currentNumber = '123456'
+    component.clear()
+    expect(component.currentNumber).toBe('0')
+    expect(component.firstOperand).toBeNull()
+    expect(component.operator).toBeNull()
+    expect(component.pauseForSecondNumber).toBeFalse()
+  })
+
+  it('should chain operations correctly', () => {
+    component.firstOperand = 10
+    expect(component.doCalculation(Operator.Add, 10)).toEqual(20)
+    expect(component.doCalculation(Operator.Subtract, 10)).toEqual(10)
+    expect(component.doCalculation(Operator.Multiply, 10)).toEqual(100)
+    expect(component.doCalculation(Operator.Divide, 10)).toEqual(10)
+  })
+  
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
 import { Component } from '@angular/core';
 
+export enum Operator {
+  Add = '+',
+  Subtract = '-',
+  Multiply = '*',
+  Divide = '/',
+  Equals = '='
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'calculator';
+
+  currentNumber: string = '0';
+  firstOperand: number | null = null;
+  operator: Operator | null = null;
+  pauseForSecondNumber: boolean = false;
+
+  get Operator() {
+    return Operator
+  }
+
+  getNumber(number: string): void {
+    if (this.pauseForSecondNumber) {
+      this.currentNumber = number;
+      this.pauseForSecondNumber = false;
+    } else {
+      this.currentNumber === '0'
+        ? (this.currentNumber = number)
+        : (this.currentNumber += number);
+    }
+  }
+
+  addDecimal(): void {
+    if (!this.currentNumber.includes('.')) {
+      this.currentNumber += '.';
+    }
+  }
+
+  doCalculation(operator: Operator, secondOperand: number): number | null {
+    if (!this.firstOperand) {
+      return null;
+    }
+    switch (operator) {
+      case Operator.Add:
+        return (this.firstOperand += secondOperand);
+      case Operator.Subtract:
+        return (this.firstOperand -= secondOperand);
+      case Operator.Multiply:
+        return (this.firstOperand *= secondOperand);
+      case Operator.Divide:
+        return (this.firstOperand /= secondOperand);
+      case Operator.Equals:
+        return secondOperand;
+    }
+  }
+
+  getOperation(operator: Operator): void {
+    if (this.firstOperand === null) {
+      this.firstOperand = Number(this.currentNumber);
+    } else if (this.operator) {
+      const result = this.doCalculation(
+        this.operator,
+        Number(this.currentNumber)
+      );
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = operator;
+    this.pauseForSecondNumber = true;
+  }
+
+  clear(): void {
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.pauseForSecondNumber = false;
+  }
 }
